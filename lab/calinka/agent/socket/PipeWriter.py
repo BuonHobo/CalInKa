151cbbe7dc59519ff8e00604c92a978f9a244ca4
buonhobo <bnnalx@gmail.com>
2024-07-24 11:47:42 +0200

    ignore

---
+++ lab/calinka/agent/socket/PipeWriter.py
@@ -0,0 +1,21 @@
+import multiprocessing.connection
+import os
+from pathlib import Path
+import asyncio
+
+from common.dispatch.IPacketLauncher import IPacketLauncher
+from common.packet.messages import Packet
+
+
+class PipeWriter(IPacketLauncher):
+    def __init__(self, output_pipe_path: Path):
+        output_pipe_path.parent.mkdir(parents=True, exist_ok=True, mode=0o600)
+        output_pipe_path.unlink(missing_ok=True)
+        os.mkfifo(output_pipe_path, 0o600)
+
+        self.__output_pipe_path = output_pipe_path
+
+    async def send(self, packet: Packet):
+        self.__output_pipe_path.write_text(
+            packet.to_json(), encoding="utf-8", newline=""
+        )
