36173dfee159c1f08d735f6ebba9654477400f10
buonhobo <bnnalx@gmail.com>
2024-07-24 12:01:17 +0200

    ignore

--- lab/calinka/common/dispatch/IPacketLauncher.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-from common.packet.messages import Packet
-
-
-class IPacketLauncher(ABC):
-    @abstractmethod
-    async def send(self, packet: Packet):
-        pass
